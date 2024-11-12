d11fdf62fd0289d5d682fb9f74b3f0dd3865e976
Jagan <jagannaath786@gmail.com>
2024-11-12 21:54:53 +0530

    Implemented sorting

--- src/main/java/dev/jagan/productserviceproject/services/SearchService.java
+++ src/main/java/dev/jagan/productserviceproject/services/SearchService.java
@@ -5,6 +5,7 @@ import dev.jagan.productserviceproject.repositories.ProductRepo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -17,6 +18,8 @@ public class SearchService {
     }
 
     public Page<Product> search(String query, int pageNumber, int pageSize){
+        Sort sort = Sort.by("title").descending().and(Sort.by("price").ascending());
+
         Pageable pageable = PageRequest.of(pageNumber,pageSize);
         return productRepo.findByTitleContaining(query,pageable);
     }
